af6dcd700ec6364a41d429b0014c0e76ff3bee75
maxth <maxthorstorp@gmail.com>
2024-08-22 16:25:54 +0200

    first commit

---
+++ Models/DirectorItem.cs
@@ -0,0 +1,9 @@
+namespace MovieCardsApi.Models;
+
+public class DirectorItem
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public DateOnly DateOfBirth { get; set; }
+}
---
+++ Models/GenreItem.cs
@@ -0,0 +1,7 @@
+namespace MovieCardsApi.Models;
+
+public class GenreItem
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
